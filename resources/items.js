2202d3a7700760b5f4f7568fb2ceb85399a24726
Jonas Stendahl <jonas.stendahl@outlook.com>
2018-04-18 09:47:45 +0200

    add items and categories

---
+++ resources/items.js
@@ -0,0 +1,152 @@
+export const shirt = {
+  dateProcessed: new Date('2018-04-01T10:26:58.779Z'),
+  traderaItemId: '306908851',
+  container: 'TQKEgpOx9i',
+  dateExported: new Date('2018-04-01T10:37:47.484Z'),
+  processingStatus: 'described',
+  itemStatus: 'utlagd',
+  section: '1',
+  freightPrice: 30,
+  site: 'A',
+  bag: 'Oz3ZUKBYEK',
+  sellpyItemId: 1764242,
+  nrOfTimesOnTradera: 1,
+  sellper: 'Qbf1tJvJvT',
+  user: 'Kw4PPAotLa',
+  traderaCategoryId: '343218',
+  dateAuctionEnds: new Date('2018-04-08T20:38:50.999Z'),
+  photographedAt: new Date('2018-04-01T09:00:55.443Z'),
+  updatedAt: new Date('2018-04-17T22:33:26.401Z'),
+  bodyText: 'Polo Ralph Lauren, Skjorta, \nModell: Custom fit, \nStrl: S, \nFärg: Beige, Flerfärgad\n\nVaran är i normalt begagnat skick.',
+  putOnShelfAt: new Date('2018-04-04T12:16:57.949Z'),
+  metadata: {
+    type: 'Skjorta',
+    brand: 'Polo Ralph Lauren',
+    color: [
+      'Beige',
+      'Flerfärgad'
+    ],
+    size: 'S',
+    model: 'Custom fit',
+    condition: 'Normalt'
+  },
+  itemPackaging: 'pHkPMEKb1f',
+  photographedBy: 'EfFtUwoM1Z',
+  putOnShelfBy: 'WkoyI6Zx4u',
+  headline: 'Polo Ralph Lauren, Skjorta, Custom fit, Strl: S, Beige/Flerfärgad',
+  estimateBid: 0.7884985128255655,
+  salesChannel: 'market',
+  images: [
+    'https://sellpy-parse-prod-files.s3.amazonaws.com/photoRobot-enterprise/resizedc-2dMWC5rMUS-a6e5-0.jpg',
+    'https://sellpy-parse-prod-files.s3.amazonaws.com/photoRobot-enterprise/resizedc-2dMWC5rMUS-a6e5-45.jpg',
+    'https://sellpy-parse-prod-files.s3.amazonaws.com/photoRobot-enterprise/resizedc-2dMWC5rMUS-a6e5-180.jpg',
+    'https://sellpy-parse-prod-files.s3.eu-west-1.amazonaws.com/phone-images/2dMWC5rMUS-56bd.jpg',
+    'https://sellpy-parse-prod-files.s3.eu-west-1.amazonaws.com/phone-images/2dMWC5rMUS-8dae.jpg'
+  ],
+  weight: 0.24,
+  currentValue: 300,
+  objectId: '2dMWC5rMUS',
+  freightCompany: 'Posten',
+  createdAt: new Date('2018-03-31T14:13:35.775Z')
+}
+
+export const shoes = {
+  dateProcessed: new Date('2018-03-22T09:31:38.488Z'),
+  container: '31xuxmdffn',
+  processingStatus: 'described',
+  itemStatus: 'utlagd',
+  section: '1',
+  freightPrice: 75,
+  site: 'A',
+  bag: 'D2lXAtZ1Nz',
+  sellpyItemId: 1776566,
+  sellper: 'jbcd9HiGsG',
+  user: 'Z9EgUawPFj',
+  traderaCategoryId: '342400',
+  photographedAt: new Date('2018-03-22T07:21:50.775Z'),
+  updatedAt: new Date('2018-04-09T16:35:49.550Z'),
+  bodyText: 'Acne Studios, Boots, \nStrl: 36, \nFärg: Svart, \nMaterial: Skinn, \nKlackhöjd i cm: 7\n\nVaran är i normalt begagnat skick.',
+  putOnShelfAt: new Date('2018-04-09T16:35:49.530Z'),
+  metadata: {
+    type: 'Boots',
+    brand: 'Acne Studios',
+    color: [
+      'Svart'
+    ],
+    material: [
+      'Skinn'
+    ],
+    size: '36',
+    condition: 'Normalt',
+    extra: 'Klackhöjd i cm: 7'
+  },
+  itemPackaging: 'iJdt9bjl4w',
+  photographedBy: 'eUKQgfIkg5',
+  putOnShelfBy: 'RPHWhHqJ1b',
+  headline: 'Acne Studios, Boots, Strl: 36, Svart, Skinn',
+  estimateBid: 0.9727105414503573,
+  salesChannel: 'market',
+  images: [
+    'https://sellpy-parse-prod-files.s3.amazonaws.com/photoRobot-case-bondaroff/resizedc-cqRPkZVsYM-46bc-0.jpg',
+    'https://sellpy-parse-prod-files.s3.amazonaws.com/photoRobot-case-bondaroff/resizedc-cqRPkZVsYM-46bc-90.jpg',
+    'https://sellpy-parse-prod-files.s3.amazonaws.com/photoRobot-case-bondaroff/resizedc-cqRPkZVsYM-46bc-180.jpg',
+    'https://sellpy-parse-prod-files.s3.amazonaws.com/photoRobot-case-bondaroff/resizedc-cqRPkZVsYM-46bc-270.jpg',
+    'https://sellpy-parse-prod-files.s3.eu-west-1.amazonaws.com/phone-images/cqRPkZVsYM-a6da.jpg'
+  ],
+  weight: 1.19,
+  currentValue: 1000,
+  objectId: '3ZrZX0O7Gh',
+  freightCompany: 'Schenker',
+  createdAt: new Date('2018-04-09T16:09:29.371Z')
+}
+
+export const iphone = {
+  dateProcessed: new Date('2018-03-29T13:42:19.852Z'),
+  traderaItemId: '306712984',
+  container: 'nUYef00SXl',
+  dateExported: new Date('2018-03-29T17:33:10.270Z'),
+  processingStatus: 'described',
+  itemStatus: 'utlagd',
+  section: '1',
+  freightPrice: 52,
+  site: 'B',
+  bag: 'FXhAsS9qny',
+  sellpyItemId: 236753,
+  nrOfTimesOnTradera: 2,
+  sellper: 'c0sZJZeVpp',
+  user: 'Q3cKl3dfXT',
+  traderaCategoryId: '340186',
+  dateAuctionEnds: new Date('2018-04-08T19:54:11.999Z'),
+  photographedAt: new Date('2018-03-29T06:26:36.649Z'),
+  updatedAt: new Date('2018-04-17T21:33:54.089Z'),
+  bodyText: 'Apple, iPhone 6, \nModell: A1586, 64GB, Olåst, \nFärg: Silverfärgad\n\nVaran är i fint begagnat skick. Vissa tecken på användning finns (Mikrofonen fungerar ej, kommer utan tillbehör)',
+  putOnShelfAt: new Date('2018-04-04T10:07:37.350Z'),
+  metadata: {
+    type: 'iPhone 6',
+    brand: 'Apple',
+    color: [
+      'Silverfärgad'
+    ],
+    model: 'A1586, 64GB, Olåst',
+    condition: 'Fint',
+    defect: 'Mikrofonen fungerar ej, kommer utan tillbehör'
+  },
+  itemPackaging: 'iJdt9bjl4w',
+  photographedBy: 'c0sZJZeVpp',
+  putOnShelfBy: '6qCQRUWzaX',
+  headline: 'Apple, iPhone 6, A1586, 64GB, Olåst, Silverfärgad',
+  estimateBid: 0.8381081362892775,
+  salesChannel: 'market',
+  images: [
+    'https://sellpy-parse-prod-files.s3.eu-west-1.amazonaws.com/phone-images/9yBpgFIqMl-e65f.jpg',
+    'https://sellpy-parse-prod-files.s3.eu-west-1.amazonaws.com/phone-images/9yBpgFIqMl-f485.jpg',
+    'https://sellpy-parse-prod-files.s3.eu-west-1.amazonaws.com/phone-images/9yBpgFIqMl-fc79.jpg',
+    'https://sellpy-parse-prod-files.s3.eu-west-1.amazonaws.com/phone-images/9yBpgFIqMl-0158.jpg',
+    'https://sellpy-parse-prod-files.s3.eu-west-1.amazonaws.com/phone-images/9yBpgFIqMl-06ec.jpg'
+  ],
+  weight: 0.46,
+  currentValue: 2500,
+  objectId: '9yBpgFIqMl',
+  freightCompany: 'Schenker',
+  createdAt: new Date('2018-03-28T11:29:02.575Z')
+}
