b602423836fd2bfa339b93f4f4e9cf54048b74ae
Jonas Stendahl <jonas@sellpy.se>
2018-04-25 11:27:52 +0200

    fix standard js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {shirt} from './resources/items'
 import './App.css';
+
+const columnStyle = {
+  flexBasis: '40%',
+  minWidth: '300px'
+}
+
+const ImageContainer = ({images}) => {
+  const imageStyle = {
+    width: '100%'
+  }
+  return <div
+      style={columnStyle}
+    >
+      <img 
+      src={images[0]}
+      alt='Product'
+      style={imageStyle}
+    />
+  </div>
+}
+
+const ItemInfo = ({item}) => {
+  const style = {
+    ...columnStyle,
+    display: 'flex',
+    flexDirection: 'column'
+  }
+  return <div
+    style={style}
+  >
+    <p>{item.metadata.brand}</p>
+    <p>{item.metadata.type}</p>
+    <p>{item.currentValue}</p>
+  </div>
+}
+
+const ItemDetailPage = ({item}) => {
+  const style = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around'
+  }
+  return <div style={style}>
+    <ImageContainer
+      images={item.images}
+    />
+    <ItemInfo
+      item={item}
+    />
+  </div>
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <ItemDetailPage
+      item={shirt}
+    />
   }
 }
 
