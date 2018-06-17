ef5fd3a1434657646cafa6d1457c41c74accf55d
Samir Chaabi <samir.chaabi@netlight.com>
2018-06-17 15:17:46 +0200

    Add App with all components and include stylesheet

--- src/App.js
+++ src/App.js
@@ -1,62 +1,23 @@
-import React, {Component} from 'react'
-import {shirt} from './resources/items'
+import React, {Component} from 'react';
+import {shirt} from './resources/items';
 
-const columnStyle = {
-  flexBasis: '40%',
-  minWidth: '300px'
-}
-
-const ImageContainer = ({images}) => {
-  const imageStyle = {
-    width: '100%'
-  }
-  return <div
-      style={columnStyle}
-    >
-      <img 
-      src={images[0]}
-      alt='Product'
-      style={imageStyle}
-    />
-  </div>
-}
-
-const ItemInfo = ({item}) => {
-  const style = {
-    ...columnStyle,
-    display: 'flex',
-    flexDirection: 'column'
-  }
-  return <div
-    style={style}
-  >
-    <p>{item.metadata.brand}</p>
-    <p>{item.metadata.type}</p>
-    <p>{item.currentValue}</p>
-  </div>
-}
-
-const ItemDetailPage = ({item}) => {
-  const style = {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around'
-  }
-  return <div style={style}>
-    <ImageContainer
-      images={item.images}
-    />
-    <ItemInfo
-      item={item}
-    />
-  </div>
-}
+import Header from './components/header/headerContainer';
+import BreadCrumbs from './components/breadCrumbs/breadCrumbsContainer';
+import ItemDetailedPage from './components/item/itemContainer';
+import Footer from './components/footer/footerContainer';
 
 class App extends Component {
   render() {
-    return <ItemDetailPage
-      item={shirt}
-    />
+    return (
+        <div>
+            <Header />
+            <div className = "main">
+                <BreadCrumbs />
+                <ItemDetailedPage { ...shirt } />
+            </div>
+            <Footer />
+        </div>
+    )
   }
 }
 
